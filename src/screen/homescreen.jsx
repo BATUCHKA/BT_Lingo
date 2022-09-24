ca44f6697170b0d54840d7e9e9c0a39fc1b8f9ee
Bataa Boldbaatar <bataaboldbaatar1@gmail.com>
2022-09-25 00:38:02 +0800

    there confict on app

---
+++ src/screen/homescreen.jsx
@@ -0,0 +1,15 @@
+import {React} from 'react';
+import '../style/home.css';
+
+
+console.log('homescreen bn gici mine');
+
+export const HomeScreen = () => {
+    return (
+        <div style={{'height': '100%'}} color='black'>
+            <text color='black'>
+                Please i am here
+            </text>
+        </div>
+    )
+}
